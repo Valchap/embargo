62d78972bb1f16215268d183936d19e9811eae35
valchap <valchap616@gmail.com>
2023-04-01 19:32:09 +0200

    Multithreaded build

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@
 
 mod parallel_runner;
 
-use std::process::Command;
+use std::{fs::create_dir_all, path::Path, process::Command};
 
+use parallel_runner::parallel_run;
 use toml::{map::Map, Table, Value};
 use walkdir::WalkDir;
 
@@ -103,7 +104,7 @@ fn read_string_list_key(
     }
 }
 
-fn to_owned_string_list(in_list: &[&str]) -> Vec<String> {
+fn to_owned_string_vec(in_list: &[&str]) -> Vec<String> {
     let mut out_list = Vec::new();
 
     for &s in in_list {
@@ -111,6 +112,18 @@ fn to_owned_string_list(in_list: &[&str]) -> Vec<String> {
     }
 
     out_list
+}
+
+fn default_configuration() -> Config {
+    Config {
+        compiler: DEFAULT_COMPILER.to_owned(),
+        debugger: DEFAULT_DEBUGGER.to_owned(),
+        linter: DEFAULT_LINTER.to_owned(),
+        flags: to_owned_string_vec(DEFAULT_FLAGS),
+        debug_flags: to_owned_string_vec(DEFAULT_DEBUG_FLAGS),
+        release_flags: to_owned_string_vec(DEFAULT_RELEASE_FLAGS),
+        linter_checks: DEFAULT_LINTER_CHECKS.to_owned(),
+    }
 }
 
 fn read_configuration(config_path: &str) -> Result<Config, String> {
@@ -125,11 +138,11 @@ fn read_configuration(config_path: &str) -> Result<Config, String> {
                     read_string_key(&toml, LINTER_KEY)?.unwrap_or(DEFAULT_LINTER.to_owned());
 
                 let flags = read_string_list_key(&toml, FLAGS_KEY)?
-                    .unwrap_or(to_owned_string_list(DEFAULT_FLAGS));
+                    .unwrap_or(to_owned_string_vec(DEFAULT_FLAGS));
                 let debug_flags = read_string_list_key(&toml, DEBUG_FLAGS_KEY)?
-                    .unwrap_or(to_owned_string_list(DEFAULT_DEBUG_FLAGS));
+                    .unwrap_or(to_owned_string_vec(DEFAULT_DEBUG_FLAGS));
                 let release_flags = read_string_list_key(&toml, RELEASE_FLAGS_KEY)?
-                    .unwrap_or(to_owned_string_list(DEFAULT_RELEASE_FLAGS));
+                    .unwrap_or(to_owned_string_vec(DEFAULT_RELEASE_FLAGS));
 
                 let linter_checks = read_string_key(&toml, LINTER_CHECKS_KEY)?
                     .unwrap_or(DEFAULT_LINTER_CHECKS.to_owned());
@@ -192,50 +205,150 @@ fn find_code() -> Result<Vec<String>, String> {
     find_file(SRC_DIR, &[".hpp", ".h", ".cpp", ".c"])
 }
 
-fn build(compiler: &str, flags: &[&str], build_subdir: &str) {
-    let source_files = match find_srcs() {
-        Ok(srcs) => srcs,
-        Err(error) => {
-            eprintln!("{error}");
-            return;
-        }
-    };
+fn find_objects(build_subdir: &str) -> Result<Vec<String>, String> {
+    find_file(&format!("{BUILD_DIR}{SEPARATOR}{build_subdir}"), &[".o"])
+}
 
-    if !std::path::Path::new(BUILD_DIR).is_dir() {
-        if let Err(error) = std::fs::create_dir(BUILD_DIR) {
-            eprintln!("Can't create {BUILD_DIR} directory : {error}");
-            return;
-        }
-    }
-
-    let subdir = format!("{BUILD_DIR}{SEPARATOR}{build_subdir}");
-    if !std::path::Path::new(&subdir).is_dir() {
-        if let Err(error) = std::fs::create_dir(&subdir) {
-            eprintln!("Can't create {subdir} directory : {error}");
-            return;
-        }
-    }
+fn compile_object(options: (String, Vec<String>, String, String)) -> Result<bool, String> {
+    let compiler = options.0;
+    let flags = options.1;
+    let input = options.2;
+    let output = options.3;
 
     let mut compile_command = Command::new(compiler);
 
     compile_command.args(flags);
-    compile_command.arg(format!("-o{subdir}{SEPARATOR}app{EXE_EXTENSION}"));
-    compile_command.args(source_files);
+    compile_command.arg("-c");
+    compile_command.arg(format!("-o{output}"));
+    compile_command.arg(input);
+
+    let path = Path::new(&output);
+    if let Some(parent_dir) = path.parent() {
+        if let Err(error) = create_dir_all(parent_dir) {
+            return Err(format!("Can't create build folder : {error}"));
+        }
+    }
 
     let compile_result = compile_command.status();
 
     match compile_result {
-        Ok(exit_status) => {
-            if exit_status.success() {
-                println!("App built successfully");
+        Ok(compile_output) => {
+            if compile_output.success() {
+                Ok(true)
             } else {
-                println!("Compilation error !");
+                Ok(false)
             }
         }
 
+        Err(error) => Err(format!("Can't run compiler : {error}")),
+    }
+}
+
+fn compile_all_objects(compiler: &str, flags: &[&str], build_subdir: &str) -> Result<bool, String> {
+    let source_files = match find_srcs() {
+        Ok(srcs) => srcs,
         Err(error) => {
-            eprintln!("Compiler error : {error}");
+            return Err(error);
         }
+    };
+
+    let mut compile_parameters = Vec::new();
+
+    for source_file in source_files {
+        let compiler_s = compiler.to_owned();
+        let mut flags_s = Vec::new();
+        for &flag in flags {
+            flags_s.push(flag.to_owned());
+        }
+        let input_s = source_file;
+        let output_s = format!(
+            "{BUILD_DIR}{SEPARATOR}{build_subdir}{SEPARATOR}{}",
+            Path::new(&input_s).with_extension("o").to_string_lossy()
+        );
+
+        compile_parameters.push((compiler_s, flags_s, input_s, output_s));
+    }
+
+    let results = parallel_run(compile_parameters, compile_object);
+
+    for result in results {
+        match result {
+            Ok(build_successful) => {
+                if !build_successful {
+                    return Ok(false);
+                }
+            }
+            Err(compiler_error) => return Err(compiler_error),
+        }
+    }
+
+    Ok(true)
+}
+
+fn link_program(compiler: &str, flags: &[&str], build_subdir: &str) -> Result<bool, String> {
+    let obj_files = match find_objects(build_subdir) {
+        Ok(objects) => objects,
+        Err(error) => {
+            return Err(error);
+        }
+    };
+
+    let subdir = format!("{BUILD_DIR}{SEPARATOR}{build_subdir}");
+    if let Err(error) = create_dir_all(&subdir) {
+        return Err(format!("Can't create {subdir} directory : {error}"));
+    }
+
+    let mut link_command = Command::new(compiler);
+
+    link_command.args(flags);
+    link_command.arg(format!("-o{subdir}{SEPARATOR}app{EXE_EXTENSION}"));
+    link_command.args(obj_files);
+
+    let link_result = link_command.status();
+
+    match link_result {
+        Ok(exit_status) => {
+            if exit_status.success() {
+                Ok(true)
+            } else {
+                Ok(false)
+            }
+        }
+
+        Err(error) => Err(format!("Compiler error : {error}")),
+    }
+}
+
+fn build(config: &Config, release: bool) -> Result<bool, String> {
+    let mut flags = Vec::<&str>::new();
+
+    for f in &config.flags {
+        flags.push(f);
+    }
+
+    if release {
+        for f in &config.release_flags {
+            flags.push(f);
+        }
+    } else {
+        for f in &config.debug_flags {
+            flags.push(f);
+        }
+    }
+
+    let include_flag = format!("-I{INCLUDE_DIR}");
+    flags.push(&include_flag);
+
+    let build_subdir = if release {
+        RELEASE_BUILD_SUBDIR
+    } else {
+        DEBUG_BUILD_SUBDIR
+    };
+
+    if compile_all_objects(&config.compiler, &flags, build_subdir)? {
+        link_program(&config.compiler, &flags, build_subdir)
+    } else {
+        Ok(false)
     }
 }
 
@@ -272,81 +385,56 @@ fn lint(linter: &str, checks: &str, compile_flags: &[&str]) {
     }
 }
 
-fn build_command() {
-    match read_configuration(".") {
-        Ok(config) => {
-            let mut flags = Vec::<&str>::new();
-
-            for f in &config.flags {
-                flags.push(f);
+fn build_command(config: &Config) {
+    match build(config, false) {
+        Ok(successful) => {
+            if successful {
+                println!("Build successful");
+            } else {
+                println!("Build failed");
             }
-
-            for f in &config.debug_flags {
-                flags.push(f);
-            }
-
-            let f = format!("-I{INCLUDE_DIR}");
-            flags.push(&f);
-
-            build(&config.compiler, &flags, DEBUG_BUILD_SUBDIR);
         }
         Err(err_msg) => {
-            eprintln!("{err_msg}");
+            eprintln!("Internal error : {err_msg}");
         }
     }
 }
 
-fn release_build_command() {
-    match read_configuration(".") {
-        Ok(config) => {
-            let mut flags = Vec::<&str>::new();
-
-            for f in &config.flags {
-                flags.push(f);
+fn release_build_command(config: &Config) {
+    match build(config, true) {
+        Ok(successful) => {
+            if successful {
+                println!("Build successful");
+            } else {
+                println!("Build failed");
             }
-
-            for f in &config.debug_flags {
-                flags.push(f);
-            }
-
-            let f = format!("-I{INCLUDE_DIR}");
-            flags.push(&f);
-
-            build(&config.compiler, &flags, RELEASE_BUILD_SUBDIR);
         }
         Err(err_msg) => {
-            eprintln!("{err_msg}");
+            eprintln!("Internal error : {err_msg}");
         }
     }
 }
 
-fn run_command() {
-    build_command();
+fn run_command(config: &Config) {
+    build_command(config);
 
-    match read_configuration(".") {
-        Ok(config) => {
-            let mut run_command = Command::new(config.debugger);
-            run_command.arg("--source-quietly");
-            run_command.arg("-o");
-            run_command.arg("run");
-            run_command.arg("-o");
-            run_command.arg("exit");
-            run_command.arg(format!(
-                "{BUILD_DIR}{SEPARATOR}{DEBUG_BUILD_SUBDIR}{SEPARATOR}app{EXE_EXTENSION}"
-            ));
+    let mut run_command = Command::new(&config.debugger);
+    run_command.arg("--source-quietly");
+    run_command.arg("-o");
+    run_command.arg("run");
+    run_command.arg("-o");
+    run_command.arg("exit");
+    run_command.arg(format!(
+        "{BUILD_DIR}{SEPARATOR}{DEBUG_BUILD_SUBDIR}{SEPARATOR}app{EXE_EXTENSION}"
+    ));
 
-            if let Err(error) = run_command.status() {
-                println!("Can't run your app : {error}");
-            }
-        }
-        Err(err_msg) => {
-            eprintln!("{err_msg}");
-        }
+    if let Err(error) = run_command.status() {
+        println!("Can't run your app : {error}");
     }
 }
 
-fn release_run_command() {
-    release_build_command();
+fn release_run_command(config: &Config) {
+    release_build_command(config);
 
     let mut run_command = Command::new(format!(
         "{BUILD_DIR}{SEPARATOR}{RELEASE_BUILD_SUBDIR}{SEPARATOR}app{EXE_EXTENSION}"
@@ -357,44 +445,30 @@ fn release_run_command() {
     }
 }
 
-fn debug_command() {
-    build_command();
+fn debug_command(config: &Config) {
+    build_command(config);
 
-    match read_configuration(".") {
-        Ok(config) => {
-            let mut run_command = Command::new(config.debugger);
-            run_command.arg(format!(
-                "{BUILD_DIR}{SEPARATOR}{DEBUG_BUILD_SUBDIR}{SEPARATOR}app{EXE_EXTENSION}"
-            ));
+    let mut run_command = Command::new(&config.debugger);
+    run_command.arg(format!(
+        "{BUILD_DIR}{SEPARATOR}{DEBUG_BUILD_SUBDIR}{SEPARATOR}app{EXE_EXTENSION}"
+    ));
 
-            if let Err(error) = run_command.status() {
-                println!("Can't run your app : {error}");
-            }
-        }
-        Err(err_msg) => {
-            eprintln!("{err_msg}");
-        }
+    if let Err(error) = run_command.status() {
+        println!("Can't run your app : {error}");
     }
 }
 
-fn lint_command() {
-    match read_configuration(".") {
-        Ok(config) => {
-            let mut flags = Vec::<&str>::new();
+fn lint_command(config: &Config) {
+    let mut flags = Vec::<&str>::new();
 
-            for f in &config.flags {
-                flags.push(f);
-            }
-
-            let f = format!("-I{INCLUDE_DIR}");
-            flags.push(&f);
-
-            lint(&config.linter, &config.linter_checks, &flags);
-        }
-        Err(err_msg) => {
-            eprintln!("{err_msg}");
-        }
+    for f in &config.flags {
+        flags.push(f);
     }
+
+    let f = format!("-I{INCLUDE_DIR}");
+    flags.push(&f);
+
+    lint(&config.linter, &config.linter_checks, &flags);
 }
 
 fn init_command() {
@@ -429,48 +503,34 @@ fn init_command() {
         if let Err(error) = std::fs::write(CONFIG_FILE, "") {
             eprintln!("Can't create {CONFIG_FILE} : {error}");
         } else {
-            clangd_config_command();
+            clangd_config_command(&default_configuration());
         }
     }
 }
 
-fn config_command() {
-    match read_configuration(".") {
-        Ok(config) => {
-            println!("Embargo is configured as follow: ");
-            println!("    Compiler          {}", config.compiler);
-            println!("    Debugger          {}", config.debugger);
-            println!("    Linter            {}", config.linter);
-            println!("    Flags             {:?}", config.flags);
-            println!("    Debug flags       {:?}", config.debug_flags);
-            println!("    Release flags     {:?}", config.release_flags);
-        }
-        Err(err_msg) => {
-            eprintln!("{err_msg}");
-        }
-    }
+fn show_config_command(config: &Config) {
+    println!("Embargo is configured as follow: ");
+    println!("    Compiler          {}", config.compiler);
+    println!("    Debugger          {}", config.debugger);
+    println!("    Linter            {}", config.linter);
+    println!("    Flags             {:?}", config.flags);
+    println!("    Debug flags       {:?}", config.debug_flags);
+    println!("    Release flags     {:?}", config.release_flags);
 }
 
-fn clangd_config_command() {
-    match read_configuration(".") {
-        Ok(config) => {
-            let mut compile_flags = String::new();
+fn clangd_config_command(config: &Config) {
+    let mut compile_flags = String::new();
 
-            compile_flags.push_str("-Iinclude\n");
-            compile_flags.push_str("-Isrc\n");
+    compile_flags.push_str("-Iinclude\n");
+    compile_flags.push_str("-Isrc\n");
 
-            for flag in &config.flags {
-                compile_flags.push_str(flag);
-                compile_flags.push('\n');
-            }
+    for flag in &config.flags {
+        compile_flags.push_str(flag);
+        compile_flags.push('\n');
+    }
 
-            if let Err(error) = std::fs::write(COMPILE_FLAGS_FILE, compile_flags) {
-                eprintln!("Can't write {COMPILE_FLAGS_FILE} : {error}");
-            }
-        }
-        Err(err_msg) => {
-            eprintln!("{err_msg}");
-        }
+    if let Err(error) = std::fs::write(COMPILE_FLAGS_FILE, compile_flags) {
+        eprintln!("Can't write {COMPILE_FLAGS_FILE} : {error}");
     }
 }
 
@@ -492,7 +552,7 @@ fn help_command() {
     println!("    debug            Build the app in debug mode and open it with the debugger");
     println!("    lint             Run the linter on your project to find common mistakes");
     println!("    init             Create default files to start working on your project");
-    println!("    config           Show embargo configuration as defined in Embargo.toml file");
+    println!("    show-config      Show embargo configuration as defined in Embargo.toml file");
     println!("    clangd-config    Generate compile_flags.txt based on your configuration for clangd settings");
     println!("    clean            Remove the build directory");
     println!("    help             Show this help message");
@@ -501,18 +561,34 @@ fn help_command() {
 fn main() {
     if let Some(first_arg) = std::env::args().nth(1) {
         match first_arg.as_str() {
-            "build" => build_command(),
-            "release-build" => release_build_command(),
-            "run" => run_command(),
-            "release-run" => release_run_command(),
-            "debug" => debug_command(),
-            "lint" => lint_command(),
+            // Commands usable outside project directory
             "init" => init_command(),
-            "config" => config_command(),
-            "clangd-config" => clangd_config_command(),
-            "clean" => clean_command(),
             "help" => help_command(),
-            _ => eprintln!("Unknown command, try `embargo help` for more informations"),
+
+            _ => {
+                match read_configuration(".") {
+                    Ok(config) => {
+                        match first_arg.as_str() {
+                            // Commands for use inside a project
+                            "build" => build_command(&config),
+                            "release-build" => release_build_command(&config),
+                            "run" => run_command(&config),
+                            "release-run" => release_run_command(&config),
+                            "debug" => debug_command(&config),
+                            "lint" => lint_command(&config),
+                            "show-config" => show_config_command(&config),
+                            "clangd-config" => clangd_config_command(&config),
+                            "clean" => clean_command(), // Doesn't need configuration, but for safety can only be used inside a project
+                            _ => eprintln!(
+                                "Unknown command, try `embargo help` for more informations"
+                            ),
+                        }
+                    }
+                    Err(err_msg) => {
+                        eprintln!("{err_msg}");
+                    }
+                }
+            }
         }
     } else {
         eprintln!("Embargo takes a command as parameter, try `embargo help` for more informations");
